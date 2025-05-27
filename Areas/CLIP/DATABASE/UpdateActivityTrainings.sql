aa85cef28d7391f9eee1974beba31240ef93e718
ayman <danishaiman3b@gmail.com>
2025-05-27 10:27:36 +0800

    Remove obsolete SQL scripts related to database updates, data migration, and table creation from the CLIP database area.

--- Areas/CLIP/DATABASE/UpdateActivityTrainings.sql
+++
@@ -1,9 +0,0 @@
--- Rename Atom_CEP to Atom_CEP
-EXEC sp_rename 'AspNetUsers.CPD_Points', 'Atom_CEP', 'COLUMN';
-
--- Rename DOE_CPD to DOE_CPD
-EXEC sp_rename 'AspNetUsers.DOE_CPD', 'DOE_CPD', 'COLUMN';
-
--- Add new column Dosh_CEP with default value 0
-ALTER TABLE AspNetUsers
-ADD Dosh_CEP INT NULL CONSTRAINT DF_AspNetUsers_Dosh_CEP DEFAULT 0;
