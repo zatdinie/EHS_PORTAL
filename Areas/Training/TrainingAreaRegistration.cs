364be8d246332401354a25a764fbb7fd1e76a867
ayman <danishaiman3b@gmail.com>
2025-07-31 10:09:32 +0800

    Initialize Training Portal

---
+++ Areas/Training/TrainingAreaRegistration.cs
@@ -0,0 +1,25 @@
+using System.Web.Mvc;
+
+namespace EHS_PORTAL.Areas.Training
+{
+    public class TrainingAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Training";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Training_default",
+                "Training/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "EHS_PORTAL.Areas.Training.Controllers" }
+            );
+        }
+    }
+}
