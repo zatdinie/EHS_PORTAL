4d0e7855ce377a03aff927fe4a9ecb37fc7160bf
ayman <danishaiman3b@gmail.com>
2025-08-01 15:28:36 +0800

    Enhance Training Portal by adding new images and views for General, Environment, and Safety & Health categories; update project file to include new controllers and views.

---
+++ Areas/Training/Controllers/EnvironmentController.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace EHS_PORTAL.Areas.Training.Controllers
+{
+    public class EnvironmentController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Areas/Training/Controllers/GeneralController.cs
@@ -0,0 +1,52 @@
+using System.Web.Mvc;
+
+namespace EHS_PORTAL.Areas.Training.Controllers
+{
+    public class GeneralController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChemicalSpill()
+        {
+            return View();
+        }
+
+        public ActionResult FireExtinguisher()
+        {
+            return View();
+        }
+
+        public ActionResult PPE()
+        {
+            return View();
+        }
+
+        public ActionResult EmergencyProcedures()
+        {
+            return View();
+        }
+
+        public ActionResult WorkplaceHazards()
+        {
+            return View();
+        }
+
+        public ActionResult FirstAid()
+        {
+            return View();
+        }
+
+        public ActionResult ManualHandling()
+        {
+            return View();
+        }
+
+        public ActionResult WorkplaceSafety()
+        {
+            return View();
+        }
+    }
+} 
